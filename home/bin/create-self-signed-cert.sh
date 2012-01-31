a828316ae56994219dac0656621e6e9a18b638f3
Shaun <shaun@enmasse.com>
2012-01-31 12:19:08 -0800

    add docs, make original key extention a little more obvious

--- home/bin/create-self-signed-cert.sh
+++ home/bin/create-self-signed-cert.sh
@@ -12,8 +12,9 @@ echo 'Please provide the domain for which the cert should be named.'
   exit
 fi
 
+# From: http://www.akadia.com/services/ssh_test_certificate.html
 openssl genrsa -des3 -out $1.key 1024
 openssl req -new -key $1.key -out $1.csr
-cp $1.key $1.key.org
-openssl rsa -in $1.key.org -out $1.key
+cp $1.key $1.key.orig
+openssl rsa -in $1.key.orig -out $1.key
 openssl x509 -req -days 365 -in $1.csr -signkey $1.key -out $1.crt
